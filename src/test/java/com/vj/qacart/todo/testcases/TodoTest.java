86ae4c99fa65a39e6dac04103ee5d6cd1c71769b
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-05-16 15:44:41 +0530

    used the POJO class in our tests

--- src/test/java/com/vj/qacart/todo/testcases/TodoTest.java
+++ src/test/java/com/vj/qacart/todo/testcases/TodoTest.java
@@ -3,6 +3,7 @@ package com.vj.qacart.todo.testcases;
 import com.github.javafaker.Faker;
 import com.vj.qacart.todo.base.BaseTest;
 import com.vj.qacart.todo.factory.DriverFactory;
+import com.vj.qacart.todo.models.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,14 +18,14 @@ public class TodoTest extends BaseTest {
     public void shouldBeAbleToAddATodo() {
 
         Faker faker = new Faker();
-        String firsName = faker.name().firstName();
+        String firstName = faker.name().firstName();
         String lastName = faker.name().lastName();
         String email = faker.internet().safeEmailAddress();
-
+        User user = new User(firstName, lastName, email, "Simple1!");
         driver.get("https://todo.qacart.com/signup");
-        driver.findElement(By.cssSelector("[data-testid='first-name']")).sendKeys(firsName);
-        driver.findElement(By.cssSelector("[data-testid='last-name']")).sendKeys(lastName);
-        driver.findElement(By.cssSelector("[data-testid='email']")).sendKeys(email);
+        driver.findElement(By.cssSelector("[data-testid='first-name']")).sendKeys(user.getFirstName());
+        driver.findElement(By.cssSelector("[data-testid='last-name']")).sendKeys(user.getLastName());
+        driver.findElement(By.cssSelector("[data-testid='email']")).sendKeys(user.getEmail());
         driver.findElement(By.cssSelector("[data-testid='password']")).sendKeys("Simple1!");
         driver.findElement(By.cssSelector("[data-testid='confirm-password']")).sendKeys("Simple1!");
         driver.findElement(By.cssSelector("[data-testid='submit']")).click();
@@ -40,14 +41,14 @@ public class TodoTest extends BaseTest {
     @Test
     public void shouldBeAbleToDeleteAddedTodo() {
         Faker faker = new Faker();
-        String firsName = faker.name().firstName();
+        String firstName = faker.name().firstName();
         String lastName = faker.name().lastName();
         String email = faker.internet().safeEmailAddress();
-
+        User user = new User(firstName, lastName, email, "Simple1!");
         driver.get("https://todo.qacart.com/signup");
-        driver.findElement(By.cssSelector("[data-testid='first-name']")).sendKeys(firsName);
-        driver.findElement(By.cssSelector("[data-testid='last-name']")).sendKeys(lastName);
-        driver.findElement(By.cssSelector("[data-testid='email']")).sendKeys(email);
+        driver.findElement(By.cssSelector("[data-testid='first-name']")).sendKeys(user.getFirstName());
+        driver.findElement(By.cssSelector("[data-testid='last-name']")).sendKeys(user.getLastName());
+        driver.findElement(By.cssSelector("[data-testid='email']")).sendKeys(user.getEmail());
         driver.findElement(By.cssSelector("[data-testid='password']")).sendKeys("Simple1!");
         driver.findElement(By.cssSelector("[data-testid='confirm-password']")).sendKeys("Simple1!");
         driver.findElement(By.cssSelector("[data-testid='submit']")).click();
