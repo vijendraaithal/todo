af9bbc18c9cde6c6899ec3f86dba95acb33a3e40
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-05-16 19:30:58 +0530

    added dependency of REST Assured,used given() - .baseUri().contentType().body(), when() - post(), then() - log().all()

---
+++ src/test/java/com/vj/qacart/todo/apis/UserApi.java
@@ -0,0 +1,22 @@
+package com.vj.qacart.todo.apis;
+
+import com.vj.qacart.todo.models.User;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+
+public class UserApi {
+    public static void main(String[] args) {
+        // Given, When, Then
+        User user = new User();
+        given()
+            .baseUri("https://todo.qacart.com")
+            .contentType(ContentType.JSON)
+//            .body("{\"email\":\"super.vj.007c@email.com\",\"password\":\"Simple1!\",\"firstName\":\"John\",\"lastName\":\"Smith\"}")
+            .body(user)
+        .when()
+            .post("/api/v1/users/register")
+        .then()
+            .log().all();
+    }
+}
