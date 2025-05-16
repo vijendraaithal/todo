bc572ca0a019f591581350d452ce4e2d065c8a3c
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-05-16 16:21:10 +0530

    simplified the registration test case

--- src/test/java/com/vj/qacart/todo/pages/RegisterPage.java
+++ src/test/java/com/vj/qacart/todo/pages/RegisterPage.java
@@ -1,6 +1,8 @@
 package com.vj.qacart.todo.pages;
 
+import com.vj.qacart.todo.models.User;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class RegisterPage {
 
@@ -8,7 +10,21 @@ public class RegisterPage {
     private final By inpFirstName = By.cssSelector("[data-testid='first-name']");
     private final By inpLastName = By.cssSelector("[data-testid='last-name']");
     private final By inpEmail = By.cssSelector("[data-testid='email']");
-    private final By password = By.cssSelector("[data-testid='password']");
-    private final By confirmPassword = By.cssSelector("[data-testid='confirm-password']");
+    private final By inpPassword = By.cssSelector("[data-testid='password']");
+    private final By inpConfirmPassword = By.cssSelector("[data-testid='confirm-password']");
     private final By btnSubmit = By.cssSelector("[data-testid='submit']");
+
+    // Constructor
+    public RegisterPage() {
+
+    }
+    // Methods
+    public void register(WebDriver driver, User user) {
+        driver.findElement(inpFirstName).sendKeys(user.getFirstName());
+        driver.findElement(inpLastName).sendKeys(user.getLastName());
+        driver.findElement(inpEmail).sendKeys(user.getEmail());
+        driver.findElement(inpPassword).sendKeys("Simple1!");
+        driver.findElement(inpConfirmPassword).sendKeys("Simple1!");
+        driver.findElement(btnSubmit).click();
+    }
 }
--- src/test/java/com/vj/qacart/todo/testcases/UserTest.java
+++ src/test/java/com/vj/qacart/todo/testcases/UserTest.java
@@ -2,6 +2,7 @@ package com.vj.qacart.todo.testcases;
 
 import com.vj.qacart.todo.base.BaseTest;
 import com.vj.qacart.todo.models.User;
+import com.vj.qacart.todo.pages.RegisterPage;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -12,12 +13,7 @@ public class UserTest extends BaseTest {
     public void shouldBeAbleToRegister() {
         User user = new User();
         driver.get("https://todo.qacart.com/signup");
-        driver.findElement(By.cssSelector("[data-testid='first-name']")).sendKeys(user.getFirstName());
-        driver.findElement(By.cssSelector("[data-testid='last-name']")).sendKeys(user.getLastName());
-        driver.findElement(By.cssSelector("[data-testid='email']")).sendKeys(user.getEmail());
-        driver.findElement(By.cssSelector("[data-testid='password']")).sendKeys("Simple1!");
-        driver.findElement(By.cssSelector("[data-testid='confirm-password']")).sendKeys("Simple1!");
-        driver.findElement(By.cssSelector("[data-testid='submit']")).click();
+        new RegisterPage().register(driver, user);
         boolean isWelcomeHeaderDisplayed = driver.findElement(By.cssSelector("[data-testid='welcome']")).isDisplayed();
         Assert.assertTrue(isWelcomeHeaderDisplayed);
     }
