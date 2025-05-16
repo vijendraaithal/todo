f518d7d24e494bcdec03a7b6b55407e8b9967f9a
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-05-16 18:12:45 +0530

    updated to support multiple browser, can use mvn clean test -Dbrowser=<FF|CHROME|EDGE|SAFARI>

--- src/test/java/com/vj/qacart/todo/factory/DriverFactory.java
+++ src/test/java/com/vj/qacart/todo/factory/DriverFactory.java
@@ -2,13 +2,24 @@ package com.vj.qacart.todo.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
 public class DriverFactory {
 
     public WebDriver initDriver() {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver;
+        String browser = System.getProperty("browser", "CHROME");
+        switch(browser.toUpperCase()) {
+            case "CHROME" -> driver = new ChromeDriver();
+            case "FIREFOX" -> driver = new FirefoxDriver();
+            case "SAFARI" -> driver = new SafariDriver();
+            case "EDGE" -> driver = new EdgeDriver();
+            default -> throw new RuntimeException("Browser not supported");
+        }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
         driver.manage().window().maximize();
         return driver;
