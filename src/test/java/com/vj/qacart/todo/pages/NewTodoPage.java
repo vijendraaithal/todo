02d2d0a5048a74750b926e973c9edca44730165c
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-05-16 16:08:22 +0530

    added NewTodoPage & TodoPage

--- src/test/java/com/vj/qacart/todo/pages/NewTodoPage.java
+++ src/test/java/com/vj/qacart/todo/pages/NewTodoPage.java
@@ -1,4 +1,10 @@
 package com.vj.qacart.todo.pages;
 
+import org.openqa.selenium.By;
+
 public class NewTodoPage {
+    // Elements
+    private final By inpNewTodo = By.cssSelector("[data-testid='new-todo']");
+    private final By btnSubmit = By.cssSelector("[data-testid='submit-newTask']");
+
 }
--- src/test/java/com/vj/qacart/todo/pages/TodoPage.java
+++ src/test/java/com/vj/qacart/todo/pages/TodoPage.java
@@ -1,4 +1,12 @@
 package com.vj.qacart.todo.pages;
 
+import org.openqa.selenium.By;
+
 public class TodoPage {
+    // Elements
+    private final By msgWelcome = By.cssSelector("[data-testid='welcome']");
+    private final By icnPlus = By.cssSelector("[data-testid='add']");
+    private final By addedTodo = By.cssSelector("[data-testid='todo-text']");
+    private final By icnDelete = By.xpath("(//button[@aria-label='delete'])[2]");
+    private final By msgNoTodos = By.cssSelector("[data-testid='no-todos']");
 }
