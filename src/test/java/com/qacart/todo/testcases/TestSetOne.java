ff8039ba53f716182f476abcbfbefc9698881e2b
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-05-16 14:39:24 +0530

    initial commit

---
+++ src/test/java/com/qacart/todo/testcases/TestSetOne.java
@@ -0,0 +1,16 @@
+package com.qacart.todo.testcases;
+
+import org.testng.annotations.Test;
+
+public class TestSetOne {
+
+    @Test(priority = 1)
+    public void testCaseOne() {
+        System.out.println("TC 1");
+    }
+
+    @Test(priority = 2)
+    public void testCaseTwo() {
+        System.out.println("TC 2");
+    }
+}
---
+++ src/test/java/com/qacart/todo/testcases/TestSetTwo.java
@@ -0,0 +1,16 @@
+package com.qacart.todo.testcases;
+
+import org.testng.annotations.Test;
+
+public class TestSetTwo {
+
+    @Test(priority = 3)
+    public void testCaseThree() {
+        System.out.println("TC 3");
+    }
+
+    @Test(priority = 4)
+    public void testCaseFour() {
+        System.out.println("TC 4");
+    }
+}
