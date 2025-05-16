9d15368cff83dce1c8f5c8679c18f82ca5219aad
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-05-16 16:25:19 +0530

    modified and reduced the test cases

--- src/test/java/com/vj/qacart/todo/testcases/TodoTest.java
+++ src/test/java/com/vj/qacart/todo/testcases/TodoTest.java
@@ -2,6 +2,7 @@ package com.vj.qacart.todo.testcases;
 
 import com.vj.qacart.todo.base.BaseTest;
 import com.vj.qacart.todo.models.User;
+import com.vj.qacart.todo.pages.RegisterPage;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -12,12 +13,7 @@ public class TodoTest extends BaseTest {
     public void shouldBeAbleToAddATodo() {
         User user = new User();
         driver.get("https://todo.qacart.com/signup");
-        driver.findElement(By.cssSelector("[data-testid='first-name']")).sendKeys(user.getFirstName());
-        driver.findElement(By.cssSelector("[data-testid='last-name']")).sendKeys(user.getLastName());
-        driver.findElement(By.cssSelector("[data-testid='email']")).sendKeys(user.getEmail());
-        driver.findElement(By.cssSelector("[data-testid='password']")).sendKeys("Simple1!");
-        driver.findElement(By.cssSelector("[data-testid='confirm-password']")).sendKeys("Simple1!");
-        driver.findElement(By.cssSelector("[data-testid='submit']")).click();
+        new RegisterPage().register(driver, user);
         boolean isWelcomeHeaderDisplayed = driver.findElement(By.cssSelector("[data-testid='welcome']")).isDisplayed();
         Assert.assertTrue(isWelcomeHeaderDisplayed);
         driver.findElement(By.cssSelector("[data-testid='add']")).click();
@@ -31,12 +27,7 @@ public class TodoTest extends BaseTest {
     public void shouldBeAbleToDeleteAddedTodo() {
         User user = new User();
         driver.get("https://todo.qacart.com/signup");
-        driver.findElement(By.cssSelector("[data-testid='first-name']")).sendKeys(user.getFirstName());
-        driver.findElement(By.cssSelector("[data-testid='last-name']")).sendKeys(user.getLastName());
-        driver.findElement(By.cssSelector("[data-testid='email']")).sendKeys(user.getEmail());
-        driver.findElement(By.cssSelector("[data-testid='password']")).sendKeys("Simple1!");
-        driver.findElement(By.cssSelector("[data-testid='confirm-password']")).sendKeys("Simple1!");
-        driver.findElement(By.cssSelector("[data-testid='submit']")).click();
+        new RegisterPage().register(driver, user);
         boolean isWelcomeHeaderDisplayed = driver.findElement(By.cssSelector("[data-testid='welcome']")).isDisplayed();
         Assert.assertTrue(isWelcomeHeaderDisplayed);
         driver.findElement(By.cssSelector("[data-testid='add']")).click();
