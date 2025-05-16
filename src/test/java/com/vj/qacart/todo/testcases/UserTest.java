981ebfffeca13df680d288e5fb4740597a987b76
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-05-16 14:57:01 +0530

    added shouldBeAbleToRegister() test case with faker

---
+++ src/test/java/com/vj/qacart/todo/testcases/UserTest.java
@@ -0,0 +1,34 @@
+package com.vj.qacart.todo.testcases;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class UserTest {
+
+    @Test
+    public void shouldBeAbleToRegister() {
+        Faker faker = new Faker();
+        String firsName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String email = faker.internet().safeEmailAddress();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        driver.manage().window().maximize();
+        driver.get("https://todo.qacart.com/signup");
+        driver.findElement(By.cssSelector("[data-testid='first-name']")).sendKeys(firsName);
+        driver.findElement(By.cssSelector("[data-testid='last-name']")).sendKeys(lastName);
+        driver.findElement(By.cssSelector("[data-testid='email']")).sendKeys(email);
+        driver.findElement(By.cssSelector("[data-testid='password']")).sendKeys("Simple1!");
+        driver.findElement(By.cssSelector("[data-testid='confirm-password']")).sendKeys("Simple1!");
+        driver.findElement(By.cssSelector("[data-testid='submit']")).click();
+        boolean isWelcomeHeaderDisplayed = driver.findElement(By.cssSelector("[data-testid='welcome']")).isDisplayed();
+        Assert.assertTrue(isWelcomeHeaderDisplayed);
+        driver.quit();
+    }
+}
