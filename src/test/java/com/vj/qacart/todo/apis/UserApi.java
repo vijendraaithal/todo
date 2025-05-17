284825fae3240b48b1c5f62c23295d7214b1c3d7
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-05-17 14:44:26 +0530

    saving the service response and printing the value

--- src/test/java/com/vj/qacart/todo/apis/UserApi.java
+++ src/test/java/com/vj/qacart/todo/apis/UserApi.java
@@ -3,6 +3,7 @@ package com.vj.qacart.todo.apis;
 import com.vj.qacart.todo.models.User;
 import com.vj.qacart.todo.utils.ConfigUtils;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
 
@@ -10,7 +11,7 @@ public class UserApi {
     public static void main(String[] args) {
         // Given, When, Then
         User user = new User();
-        given()
+        Response response = given()
             .baseUri(ConfigUtils.getInstance().getBaseUrl())
             .contentType(ContentType.JSON)
 //            .body("{\"email\":\"super.vj.007c@email.com\",\"password\":\"Simple1!\",\"firstName\":\"John\",\"lastName\":\"Smith\"}")
@@ -18,6 +19,15 @@ public class UserApi {
         .when()
             .post("/api/v1/users/register")
         .then()
-            .log().all();
+//            .log().all();
+            .extract().response();
+
+        String access_token = response.path("access_token");
+        String firstName = response.path("firstName");
+        String userId = response.path("userID");
+        System.out.println(response.body().prettyPrint());
+        System.out.println(firstName);
+        System.out.println(access_token);
+        System.out.println(userId);
     }
 }
